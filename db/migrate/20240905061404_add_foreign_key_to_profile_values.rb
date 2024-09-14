f8de620aa1875991639435f9634aed62a82ee68c
YutaKakiki <yutakakiki@kakinokiyuunoMacBook-Air.local>
2024-09-15 00:32:46 +0900

    マイグレーションファイルの外部キー制約の重複を修正

--- db/migrate/20240905061404_add_foreign_key_to_profile_values.rb
+++ db/migrate/20240905061404_add_foreign_key_to_profile_values.rb
@@ -1,5 +1,9 @@
 class AddForeignKeyToProfileValues < ActiveRecord::Migration[7.1]
   def change
+    # 既存の外部キー制約を削除
+    remove_foreign_key :profile_values, :profile_fields
+
+    # 新しい外部キー制約を追加（on_delete: :cascade を追加）
     add_foreign_key :profile_values, :profile_fields, column: :profile_field_id, on_delete: :cascade
   end
 end
