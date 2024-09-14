be36cb2173331ce88d7681f23c843d6233bfc195
YutaKakiki <yutakakiki@kakinokiyuunoMacBook-Air.local>
2024-09-15 01:50:53 +0900

    renderのビルドコマンドを修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,19 +2,8 @@
 # exit on error
 set -o errexit
 
-# 必要な gems をインストール
 bundle install
-
-# アセットのクリーニングとプリコンパイル
 bundle exec rake assets:clean
 bundle exec rake assets:precompile
-
-# データベースのリセット（シードも含めて）
-# 必要に応じてコメントアウト
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop
-bundle exec rake db:create
-bundle exec rake db:schema:load
 bundle exec rake db:seed
-
-# データベースのマイグレーション
-bundle exec rake db:migrate --trace
+bundle exec rake db:migrate
